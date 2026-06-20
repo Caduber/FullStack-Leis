cb32ae5bceff3b104b469b9717eab358b17afd40
BrinoMD <164587075+BrinoMD@users.noreply.github.com>
2026-06-19 23:52:24 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import 'dotenv/config';
+import 'dotenv/config'; // Carrega as variáveis de ambiente do arquivo .env
 import app from './src/app.js';
 
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`); 
 });
